43bbe5444836dffe31325b42b409f510841ed617
Kezhen Liu <kl2313@eews304-005.ee.ic.ac.uk>
2017-03-11 12:50:05 +0000

    Update init to include lib, update Q3 part2

--- RF_2017/Q3.m
+++ RF_2017/Q3.m
@@ -7,3 +7,40 @@ init;
 % Set 'showImg' in getData.m to 0 to stop displaying training and testing images and their feature vectors
 [data_train, data_test] = getData('Caltech');
 close all;
+
+save('TR_TE_data.mat','data_train','data_test');
+       
+%load('TR_TE_data.mat');
+
+testLabels = cell(3);
+depthTrial = [6, 8, 10];
+numTreeTrial = [10, 50, 100];
+randomness = [10, 50, 100];
+for k = 1:3
+    for j = 1:3
+        opts = struct;
+        opts.depth = 50; 
+        opts.numTrees= numTreeTrial(k); 
+        opts.numSplits= randomness(j);  %Number of splits to try
+        opts.classifierID= 1; % which split function to be used
+        
+        %Bagging
+        Bagsize=95;
+        bagged_data_train = cell(1,opts.numTrees);
+        for i=1:opts.numTrees
+            bagged_data_train{i} = datasample(data_train,Bagsize);
+        end
+        
+        %Train several trees, use different bag for each tree
+        treeModels = cell(1, opts.numTrees);
+        for i = 1:opts.numTrees
+            %!!!Modify here for different Vocab size
+            treeModels{i} = treeTrain(bagged_data_train{i}(:,1:1024), bagged_data_train{i}(:,1025), opts); 
+        end
+        
+        %Test phase
+        %!!!Modify here for different Vocab size
+        [testLabel, testProb] = forestTest(treeModels, data_test(:,1:1024), opts);
+        testLabels{k,j} = testLabel;
+    end
+end
--- RF_2017/init.m
+++ RF_2017/init.m
@@ -7,6 +7,7 @@ close all;
 addpath('./internal');
 addpath('./external');
 addpath('./external/libsvm-3.18/matlab');
+addpath('./lib');
 
 % initialise external libraries
 run('external/vlfeat-0.9.18/toolbox/vl_setup.m'); % vlfeat library
@@ -14,4 +15,4 @@ cd('external/libsvm-3.18/matlab'); % libsvm library
 run('make');
 cd('../../..');
 
-% tested on Ubuntu 12.04, 64-bit, Intel® Core�?i7-3820 CPU @ 3.60GHz × 8 
+% tested on Ubuntu 12.04, 64-bit, Intel® Core�?i7-3820 CPU @ 3.60GHz × 8 
